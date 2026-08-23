3f15f4a1a48aadbf7575e02fee5fa6921e00cb7e
Soumyadip Mahanta <soumyadipmahanta242@gmail.com>
2026-08-23 05:51:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-8 bg-slate-900 text-white min-h-screen">
+      <h1 className="text-3xl font-bold underline text-sky-400">
+       AYOOOOO
+      </h1>
+    </div>
+  )
+}
